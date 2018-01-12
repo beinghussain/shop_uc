c6f9f76f5419c78451be28ca58e20403b74de383
hussaindehgamwala <hussain@byteprophecy.com>
2018-01-12 18:04:26 +0530

    first commit

---
+++ src/demo/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import UnderConstruction from "../lib";
+
+const App = () => (
+  <UnderConstruction
+    background={{
+      textColor: "#fff",
+      overlay: {
+        color: "#1ABC9C",
+        opacity: "1"
+      }
+    }}
+  />
+);
+
+export default App;
